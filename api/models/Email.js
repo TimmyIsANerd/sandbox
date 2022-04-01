73207b69803ecd3fddc92ce9e2340cc00a7cf851
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-04-01 12:48:23 +0100

    created model to send emails

---
+++ api/models/Email.js
@@ -0,0 +1,53 @@
+/**
+ * Email.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    title: {
+      type: "string",
+      required: true,
+      example: "Welcome to the SandBox Learning Platform",
+    },
+    fromEmail: {
+      type: "string",
+      required: true,
+    },
+    toEmail:{
+      type:'string',
+      required:true
+    },
+    message: {
+      type: "string",
+      required: true,
+    },
+    sentDate:{
+      type:'string',
+      required:true
+    },
+    messageStatus:{
+      type:'string',
+      isIn:['read','unread','failed'],
+      required:true
+    },
+    messageType:{
+      type:'string',
+      isIn:['incoming','ongoing','outbox'],
+      required:true
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+};
